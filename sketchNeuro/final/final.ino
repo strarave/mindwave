82749f8f0c16c049a10d1403c26573b51e10121b
Elia Ravella <rave@laptoprave.station>
2020-09-01 17:43:26 +0200

    finished modem setup. baudrate fixed at 57600. written "final" sketch (identical to the previous one for now, TODO: add neurosky code)

--- sketchNeuro/final/final.ino
+++ sketchNeuro/final/final.ino
@@ -1,6 +1,6 @@
 #include <SoftwareSerial.h>
 
-long int bluetoothBaud = 115200;
+long int bluetoothBaud = 57600;
 long int serialBaud = 57600;
 int boardT = 4; // TX on the modem
 int boardR = 6; // RX on the modem
@@ -12,27 +12,11 @@ void serialLog(String message){
   return;
 }
 
-void setupModem(){
-  serialLog("Entering command mode");
-  bluetooth.print("$");
-  bluetooth.print("$");
-  bluetooth.print("$");
-  
-  delay(150); // waiting for response. 150 millisecond "should work"
-  bluetooth.print("U,57600,N\n"); // changing to 57600 baud
-  
-  delay(150);
-  bluetooth.begin(serialBaud); // restarting
-  serialLog("Switched to 57600 baud");
-}
-
 void setup() {
   Serial.begin(serialBaud);
   serialLog("Serial monitor up");
   bluetooth.begin(bluetoothBaud);
   serialLog("Started bluetooth");  
-
-  setupModem();
 }
 
 void loop() {
@@ -44,9 +28,6 @@ void loop() {
   
   if(Serial.available()){
     char toSend = Serial.read();
-    
-    serialLog("Sending:");
-    serialLog((String)toSend);
     
     // Send any characters the Serial monitor prints to the bluetooth
     bluetooth.print(toSend);
