2e681d36eaefb4eb41dc6034112e954090ea2c00
Elia Ravella <rave@laptoprave.station>
2020-09-01 12:06:27 +0200

    added sketch to reset modem

--- sketchNeuro/onlyBluetoothSimple.ino/onlyBluetoothSimple.ino.ino
+++ sketchNeuro/onlyBluetoothSimple.ino/onlyBluetoothSimple.ino.ino
@@ -2,8 +2,8 @@
 
 long int bluetoothBaud = 115200;
 long int serialBaud = 9600;
-int boardT = 3;
-int boardR = 2;
+int boardT = 4;
+int boardR = 6;
 
 SoftwareSerial bluetooth(boardT, boardR);
 
@@ -12,11 +12,27 @@ void serialLog(String message){
   return;
 }
 
+void setupModem(){
+  serialLog("Entering command mode");
+  bluetooth.print("$");
+  bluetooth.print("$");
+  bluetooth.print("$");
+  
+  delay(150); // waiting for response. 150 millisecond "should work"
+  bluetooth.print("U,9600,N\n"); // changing to 9600 baud
+  
+  delay(150);
+  bluetooth.begin(serialBaud); // restarting
+  serialLog("Switched to 9600 baud");
+}
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(serialBaud);
   serialLog("Serial monitor up");
-  bluetooth.begin(115200);
+  bluetooth.begin(bluetoothBaud);
   serialLog("Started bluetooth");  
+
+  setupModem();
 }
 
 void loop() {
@@ -25,14 +41,13 @@ void loop() {
     // Send any characters the bluetooth prints to the serial monitor
     serialLog("Bluetooth modem: ");
     Serial.print((char)bluetooth.read());
-    serialLog("");  
   }
+  
   if(Serial.available()){
     char toSend = Serial.read();
     
     serialLog("Sending:");
     serialLog((String)toSend);
-    serialLog("");
     
     // Send any characters the Serial monitor prints to the bluetooth
     bluetooth.print(toSend);
